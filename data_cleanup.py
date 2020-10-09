ef19334891959f4e102b7a7a42a213c5719e7956
Julien Phillips <julien.p.333@gmail.com>
2020-10-08 21:28:58 -0400

    added cumulative data for hospitalization cases

--- data_cleanup.py
+++ data_cleanup.py
@@ -10,13 +10,30 @@ st_location_set = set(raw_search_trends_df['open_covid_region_code'].tolist())
 # print(st_location_set)
 hos_loc_filt_df = raw_hospitalization_df[raw_hospitalization_df['open_covid_region_code'].isin(st_location_set)]
 hos_loc_filt_df = hos_loc_filt_df.dropna(axis=1, how='all') # drops the columns with all null values
-# hos_loc_filt_df.to_csv("hos_loc_filt.csv", index=False)
+#hos_loc_filt_df.to_csv("hos_loc_filt.csv", index=False)
 
 #MAKE THE DAILY DATA WEEKLY DATA TO MATCH THE SEARCH TREND DATA 
 hos_loc_filt_df['date'] = pd.to_datetime(hos_loc_filt_df['date']) - pd.to_timedelta(7, unit='d')
 hos_week_loc_new_filt_df = hos_loc_filt_df.groupby(['open_covid_region_code', pd.Grouper(key='date', freq='W-MON')])['hospitalized_new'].sum().reset_index().sort_values(['open_covid_region_code','date'], ascending=[True, True])
 # hos_week_loc_new_filt_df.to_csv("hos_week_loc_new_filt.csv", index=False)
 # if we want cumulative hospitalization data, we need to calcualte it manually using the new hospitalization data. Easy to do.
+hos_week_loc_new_filt_df['hospitalized_cumulative'] = 0
+cum_hos_sum = 0
+prev_loc = 'US-AK'
+for index, row in hos_week_loc_new_filt_df.iterrows():
+    region = row['open_covid_region_code']
+    cases = row['hospitalized_new']
+    #two cases
+    #1 region is the same, so add to cumulative, set value in df
+    if region == prev_loc:
+        cum_hos_sum = cum_hos_sum + cases
+        hos_week_loc_new_filt_df.loc[index, 'hospitalized_cumulative'] = cum_hos_sum
+    #2 region is different, reset cumulative,  set value in df
+    elif region != prev_loc:
+        cum_hos_sum = 0
+        cum_hos_sum = cum_hos_sum + cases
+        hos_week_loc_new_filt_df.loc[index, 'hospitalized_cumulative'] = cum_hos_sum
+    prev_loc = region
 
 #CLEANUP SEARCH TRENDS DATA COLUMNS BEFORE MERGE
 raw_search_trends_df['date'] = pd.to_datetime(raw_search_trends_df['date']) - pd.to_timedelta(7, unit='d')
@@ -33,6 +50,17 @@ combined_df = st_cleaned_data.join(hos_week_loc_new_filt_df, how='outer')
 combined_df = combined_df.fillna(0)
 print(combined_df)
 combined_df.to_csv("combined.csv", index=True)
+
+#good reference for doing all this stuff
+#   https://stackoverflow.com/questions/12096252/use-a-list-of-values-to-select-rows-from-a-pandas-dataframe
+#   https://stackoverflow.com/questions/43297589/merge-two-data-frames-based-on-common-column-values-in-pandas
+#   https://stackoverflow.com/questions/45281297/group-by-week-in-pandas
+#   https://towardsdatascience.com/its-time-for-you-to-understand-pandas-group-by-function-cc12f7decfb9
+#   https://stackoverflow.com/questions/41815079/pandas-merge-join-two-data-frames-on-multiple-columns
+#   https://stackoverflow.com/questions/16852911/how-do-i-convert-dates-in-a-pandas-data-frame-to-a-date-data-type
+#   https://pandas.pydata.org/pandas-docs/stable/user_guide/merging.html
+#   https://stackoverflow.com/questions/40468069/merge-two-dataframes-by-index
+#   https://stackoverflow.com/questions/25478528/updating-value-in-iterrow-for-pandas
 
 
 
